bb0b8bf1ab1f10c55b3cd5a0685e83d256d50c1d
Запара Иван <truthpruthbg@gmail.com>
2025-11-28 12:54:57 +0300

    VJ-49: Добавлена сваггер дока для контроллера с навыками

--- src/main/kotlin/com/vpr42/marketplaceprofileapi/controller/SkillsController.kt
+++ src/main/kotlin/com/vpr42/marketplaceprofileapi/controller/SkillsController.kt
@@ -2,6 +2,8 @@ package com.vpr42.marketplaceprofileapi.controller
 
 import com.vpr42.marketplace.jooq.tables.pojos.Skills
 import com.vpr42.marketplaceprofileapi.service.SkillsService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
@@ -9,12 +11,17 @@ import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
 @RequestMapping("/api/profile/skills")
+@Tag(
+    name = "Навыки",
+    description = "Контроллер получения списка навыков"
+)
 class SkillsController(
     private val skillsService: SkillsService,
 ) {
     private val logger = LoggerFactory.getLogger(SkillsController::class.java)
 
     @GetMapping
+    @Operation(summary = "Получение полного списка навыков")
     fun getSkillsList(): List<Skills> {
         logger.info("Request to get skills list")
         return skillsService.getAll()
